725c894e828aa5fcc2af0ed7b9543b6a9f17b7be
Deepak Ram <deepak.ram@infocepts.com>
2023-04-14 19:47:45 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,34 @@
+from turtle import Screen
+from paddle import Paddle
+from ball import Ball
+import time
+
+screen = Screen()
+screen.setup(width=800, height=600)
+screen.bgcolor("black")
+screen.title("Pong Game")
+screen.tracer(0)
+
+l_paddle = Paddle((-350, 0))
+r_paddle = Paddle((350, 0))
+ball = Ball()
+
+screen.listen()
+screen.onkey(r_paddle.go_up, "Up")
+screen.onkey(r_paddle.go_down, "Down")
+screen.onkey(l_paddle.go_up, "w")
+screen.onkey(l_paddle.go_down, "s")
+
+game_is_on = True
+
+while game_is_on:
+    time.sleep(0.1)
+    screen.update()
+    ball.move()
+
+    # detect collision with wall
+    if ball.ycor() > 300 or ball.ycor() < -300:
+        ball.bounce()
+
+screen.exitonclick()
+
