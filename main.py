3f2b7384dc2a57118aa21df57dd024d3c8b0d427
Deepak Kumar Ram <35396750+er-deepak-ram@users.noreply.github.com>
2023-04-16 14:45:53 +0530

    customization

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from turtle import Screen
 from paddle import Paddle
 from ball import Ball
 import time
+from scoreboard import Scoreboard
 
 screen = Screen()
 screen.setup(width=800, height=600)
@@ -12,12 +13,13 @@ screen.tracer(0)
 l_paddle = Paddle((-350, 0))
 r_paddle = Paddle((350, 0))
 ball = Ball()
+scoreboard = Scoreboard()
 
 screen.listen()
-screen.onkey(r_paddle.go_up, "Up")
-screen.onkey(r_paddle.go_down, "Down")
-screen.onkey(l_paddle.go_up, "w")
-screen.onkey(l_paddle.go_down, "s")
+screen.onkeypress(r_paddle.go_up, "Up")
+screen.onkeypress(r_paddle.go_down, "Down")
+screen.onkeypress(l_paddle.go_up, "w")
+screen.onkeypress(l_paddle.go_down, "s")
 
 game_is_on = True
 
@@ -27,8 +29,22 @@ while game_is_on:
     ball.move()
 
     # detect collision with wall
-    if ball.ycor() > 300 or ball.ycor() < -300:
-        ball.bounce()
+    if ball.ycor() >= 280 or ball.ycor() <= -280:
+        ball.bounce_from_wall()
+
+    # Detect collision from paddle
+    if (ball.distance(r_paddle) < 50 and ball.xcor() >= 330) or (ball.distance(l_paddle) < 50 and ball.xcor() <= -330):
+        ball.bounce_from_paddle()
+
+    # Detect if right paddle missed the ball
+    if ball.distance(r_paddle) > 50 and ball.xcor() > 380:
+        ball.reset_position()
+        scoreboard.increment_left_score()
+
+    # Detect if left paddle missed the ball
+    elif ball.distance(l_paddle) > 50 and ball.xcor() < -380:
+        ball.reset_position()
+        scoreboard.increment_right_score()
 
 screen.exitonclick()
 
